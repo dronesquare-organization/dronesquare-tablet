96b1bf61a5eac7fdf1c6f51a3ec4889f2029d032
Han-D-Peter <secret_wings@naver.com>
2023-12-02 17:48:00 +0900

    feat: 변수명, 경로 수정

--- src/query/queries.ts
+++ src/query/queries.ts
@@ -1,30 +1,72 @@
 import { useQuery } from "@tanstack/react-query";
 import Repository from "./repository";
 
-export const useGetProjectList = () => {
+export const useGetProjects = () => {
   return useQuery({
-    queryKey: ["projectList"],
-    queryFn: () => Repository.getProjectList(),
+    queryKey: ["projects"],
+    queryFn: () => Repository.getProjects(),
   });
 };
 
-export const useGetProjectById = (id: number) => {
+export const useGetProject = (id: number) => {
   return useQuery({
-    queryKey: ["projectById", id],
-    queryFn: () => Repository.getProjectById(id),
+    queryKey: ["project", id],
+    queryFn: () => Repository.getProject(id),
   });
 };
 
-export const useGetSearchProjectList = (text: string) => {
+export const useGetSearchProjects = (text: string) => {
   return useQuery({
-    queryKey: ["searchProjectList", text],
-    queryFn: () => Repository.getSearchProjectList(text),
+    queryKey: ["searchProjects", text],
+    queryFn: () => Repository.getSearchProjects(text),
   });
 };
 
-export const useGetGisLayersById = (id: number) => {
+export const useGetAnnotations = (id: number) => {
   return useQuery({
-    queryKey: ["gisLayersById", id],
-    queryFn: () => Repository.getGisLayersById(id),
+    queryKey: ["annotations", id],
+    queryFn: () => Repository.getAnnotations(id),
+  });
+};
+
+export const useGetGisLayers = (id: number) => {
+  return useQuery({
+    queryKey: ["gisLayers", id],
+    queryFn: () => Repository.getGisLayers(id),
+  });
+};
+
+export const useGetGroupOfTimeseries = (id: number) => {
+  return useQuery({
+    queryKey: ["groupTimeseries", id],
+    queryFn: () => Repository.getGroupTimeseries(id),
+  });
+};
+
+export const useGetProjectEpsg = (id: number) => {
+  return useQuery({
+    queryKey: ["getProjectEpsg", id],
+    queryFn: () => Repository.getProjectEps(id),
+  });
+};
+
+export const useGetDxfJsonsFromS3 = (id: string) => {
+  return useQuery({
+    queryKey: ["getDxfJsons", id],
+    queryFn: () => Repository.getDxfJsonsFromS3(id),
+  });
+};
+
+export const useGetProjectZoomLevel = (id: number) => {
+  return useQuery({
+    queryKey: ["getProjectZoomLevel", id],
+    queryFn: () => Repository.getProjectZoomLevel(id),
+    // {
+    //   refetchInterval: false,
+    //   refetchOnWindowFocus: false,
+    //   refetchOnReconnect: false,
+    //   refetchOnMount: false,
+    //   retryOnMount: false,
+    //   enabled: !!id,
   });
 };
